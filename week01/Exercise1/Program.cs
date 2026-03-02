0be87787ad47c3f5865506bda060558bd6d4c726
Alvin Timana <alvinjoelts@gmail.com>
2026-03-02 17:41:37 -0500

    First commit for week01 exercises

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -4,6 +4,17 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise1 Project.");
+        // Ask the user for their first name and store it
+        Console.Write("What is your first name? ");
+        string firstName = Console.ReadLine();
+
+        // Ask the user for their last name and store it
+        Console.Write("What is your last name? ");
+        string lastName = Console.ReadLine();
+
+        Console.WriteLine(); // Blank line
+
+        // Print full name
+        Console.WriteLine($"Your name is {lastName}, {firstName} {lastName}");
     }
 }
--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -4,6 +4,45 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise2 Project.");
+        Console.WriteLine("Lets determine your grade!");
+
+        Console.Write("What is your grade percentage? ");
+        int grade = int.Parse(Console.ReadLine());
+
+        // Grades verification
+        string letter = "";
+        if (grade >= 90)
+        {
+            letter = "A";
+        }
+        else if (grade >= 80)
+        {
+            letter = "B";
+        }
+        else if (grade >= 70)
+        {
+            letter = "C";
+        }
+        else if (grade >= 60)
+        {
+            letter = "D";
+        }
+        else if (grade < 60)
+        {
+            letter = "F";
+        }
+
+        // Print letter
+        Console.WriteLine(letter);
+
+        // Determine if the user passed the course
+        if (grade >= 70)
+        {
+            Console.WriteLine("Congrats! You've passed the course.");
+        }
+        else
+        {
+            Console.WriteLine("Sadly, you didn't pass. Continue studying and you'll do it!");
+        }
     }
 }
