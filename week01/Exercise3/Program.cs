b2f67907275536ab9e9c4bec794c66dcc5c1b95a
Alvin Timana <alvinjoelts@gmail.com>
2026-03-02 18:43:02 -0500

    Challenge completed

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -4,34 +4,54 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Guess My Number!");
-        Console.WriteLine();
 
-        // Generate a random number
+        // Prepare to generate a random number
         Random randomGenerator = new Random();
-        int magicNumber = randomGenerator.Next(1, 100);
+        int magicNumber;
 
-        int guess;
-        // Game loop
+        // Initial variables
+        string play = "yes"; // Stores the user answer to play again
+        int guess; // Stores the user guess
+
+        // Start the game
         do
         {
-            Console.Write("What is your guess? ");
-            guess = int.Parse(Console.ReadLine());
+            Console.WriteLine("Guess My Number!");
+            Console.WriteLine(); // Blank line
 
-            if (guess < magicNumber)
+            // Generate a random number
+            magicNumber = randomGenerator.Next(1, 100);
+            int guessCounter = 1; // Starts with the first guess of the user
+
+            // Inside the game loop
+            do
             {
-                Console.WriteLine("Higher");
-            }
-            else if (guess > magicNumber)
-            {
-                Console.WriteLine("Lower");
-            }
-            else
-            {
-                Console.WriteLine("You guessed it!");
-            }
+                Console.Write("What is your guess? ");
+                guess = int.Parse(Console.ReadLine());
+
+                if (guess < magicNumber)
+                {
+                    Console.WriteLine("Higher");
+                }
+                else if (guess > magicNumber)
+                {
+                    Console.WriteLine("Lower");
+                }
+                else
+                {
+                    Console.WriteLine("You guessed it!");
+                    Console.WriteLine($"It took you {guessCounter} guesses.");
+                    Console.WriteLine(); // Blank line
+
+                    // Ask if the user wants to play again
+                    Console.Write("Play Again? (type 'yes' to play again) ");
+                    play = Console.ReadLine();
+                }
+
+                guessCounter ++; // Count the number of guesses
+
+            } while (guess != magicNumber);
             
-        } while (guess != magicNumber);
-
+        } while (play == "yes");
     }
 }
