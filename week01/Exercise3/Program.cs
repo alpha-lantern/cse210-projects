751b6468e6a3753e93a7c10874d161734c9ca3d4
Alvin Timana <alvinjoelts@gmail.com>
2026-03-02 18:18:26 -0500

    Week01 Exercise 3 first commit

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -4,6 +4,34 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        Console.WriteLine("Guess My Number!");
+        Console.WriteLine();
+
+        // Generate a random number
+        Random randomGenerator = new Random();
+        int magicNumber = randomGenerator.Next(1, 100);
+
+        int guess;
+        // Game loop
+        do
+        {
+            Console.Write("What is your guess? ");
+            guess = int.Parse(Console.ReadLine());
+
+            if (guess < magicNumber)
+            {
+                Console.WriteLine("Higher");
+            }
+            else if (guess > magicNumber)
+            {
+                Console.WriteLine("Lower");
+            }
+            else
+            {
+                Console.WriteLine("You guessed it!");
+            }
+            
+        } while (guess != magicNumber);
+
     }
 }
