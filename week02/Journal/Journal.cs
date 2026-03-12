1a6f2d0492bba095e224db56e51eb5b254ab9a44
Alvin Timana <alvinjoelts@gmail.com>
2026-03-11 23:16:45 -0500

    Base program finished. Main functionality working as expected (at least I think so).

---
+++ week02/Journal/Journal.cs
@@ -0,0 +1,53 @@
+public class Journal()
+{
+    // List of entries for the journal
+    public List<Entry> _entries = new List<Entry>();
+
+    // Add a new entry
+    public void AddEntry(Entry newEntry)
+    {
+        _entries.Add(newEntry);
+    }
+    // Display all the journal entries
+    public void DisplayAll()
+    {
+        foreach(Entry entry in _entries) {
+            entry.Display();
+            Console.WriteLine();
+        }
+    }
+
+    // Save the journey to a simple file
+    public bool SaveToFile(string file)
+    {
+        using (StreamWriter journalFile = new StreamWriter(file))
+        {
+            foreach(Entry entry in _entries)
+            {
+                journalFile.WriteLine($"{entry._date}~{entry._promptText}~{entry._entryText}");
+                // journalFile.WriteLine(entry._prompt);
+                // journalFile.WriteLine(entry._entry);
+            }
+        }
+        return true;
+    }
+
+    // Load from a file
+    public void LoadFromFile(string file)
+    {
+        string[] entry = File.ReadAllLines(file);
+        foreach(string line in entry)
+        {
+            // Create new entry
+            Entry newEntry = new();
+            // Split the file lines to separate the journal parts
+            string[] parts = line.Split("~");
+            // Populate new entry and add it to the list
+            newEntry._date = parts[0];
+            newEntry._promptText = parts[1];
+            newEntry._entryText = parts[2];
+            // Add to the entries list
+            AddEntry(newEntry);
+        }
+    }
+}
--- week02/Journal/Program.cs
+++ week02/Journal/Program.cs
@@ -4,6 +4,82 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Journal Project.");
+        Console.WriteLine("Welcome to the Journal Program!");
+        string choice;
+
+        // Create a Journal object to store the entries.
+        Journal currentJournal = new();
+
+        // Display the menu options
+        do
+        {
+            Console.WriteLine("Please select one of the following choices:");
+            Console.WriteLine("1. Write");
+            Console.WriteLine("2. Display");
+            Console.WriteLine("3. Load");
+            Console.WriteLine("4. Save");
+            Console.WriteLine("5. Quit");
+            Console.Write("What would you like to do? ");
+            // Read user input
+            choice = Console.ReadLine();
+
+            // Check input and respond to the option selected
+            if (choice == "1")
+            {
+                // WRITE A NEW ENTRY
+                // Generate a new Entry instance
+                Entry newEntry = new();
+
+                // Get Current Date
+                DateTime currentTime = DateTime.Now;
+                string dateText = currentTime.ToShortDateString();
+                newEntry._date = dateText;
+
+                // Get Random Prompt
+                PromptGenerator promptGen = new();
+                string prompt = promptGen.GetRandomPrompt();
+                newEntry._promptText = prompt;
+
+                // Prompt the user and store the answer as an entry
+                Console.WriteLine($"{prompt}");
+                Console.Write("> ");
+                string userEntry = Console.ReadLine();
+                newEntry._entryText = userEntry;
+
+                // Store the entry in the Journal
+                currentJournal._entries.Add(newEntry);
+            }
+            else if (choice == "2")
+            {
+                // Display the entries in the current journal
+                currentJournal.DisplayAll();
+            }
+            else if (choice == "3")
+            {
+                // Load a journal file
+                Console.WriteLine("What is the filename?");
+                string filename = Console.ReadLine();
+                currentJournal.LoadFromFile(filename);
+            }
+            else if (choice == "4")
+            {
+                // Save the current journal to a file
+                Console.WriteLine("What is the filename?");
+                string filename = Console.ReadLine();
+                currentJournal.SaveToFile(filename);
+            }
+            else if (choice == "5")
+            {
+                // Quit
+                Console.WriteLine("Good bye!");
+            }
+            else
+            {
+                // Invalid choice
+                Console.WriteLine("Invalid Choice.");
+                Console.WriteLine();
+            }
+
+        } while (choice != "5");
     }
 }
