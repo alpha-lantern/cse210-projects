a6e100e0cd14c28a54657f2ba1e25454dae1f3ec
Alvin Timana <alvinjoelts@gmail.com>
2026-04-06 21:53:06 -0500

    Shapes program completed

--- week06/Shapes/Program.cs
+++ week06/Shapes/Program.cs
@@ -1,9 +1,23 @@
-using System;
-
 class Program
 {
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Shapes Project.");
+
+        Square sqr1 = new Square("blue", 8);
+        Rectangle rect1 = new Rectangle("yellow", 4.5, 17.3);
+        Circle circle1 = new Circle("red", 1.8);
+
+        List<Shape> shapes = new List<Shape>();
+
+        shapes.Add(sqr1);
+        shapes.Add(rect1);
+        shapes.Add(circle1);
+
+        foreach (Shape shape in shapes)
+        {
+            Console.WriteLine(shape.GetColor());
+            Console.WriteLine($"Area: {shape.GetArea():F2}");
+        }
     }
 }
